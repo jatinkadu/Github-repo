abc715c8d551f107e91d4228b443893b451043db
jatinkadu <124574274+jatinkadu@users.noreply.github.com>
2024-04-06 12:41:03 +0530

    index.php

--- index.php
+++ index.php
@@ -9,20 +9,18 @@
 <body>
     <h2>Student Registration Form</h2>
     <form  method="post">
-        <label for="sid">Student ID:</label><br>
-        <input type="text" id="sid" name="sid" required><br>
 
         <label for="roll_number">roll_number:</label><br>
-        <input type="email" id="roll_number" name="roll_number" required><br>
+        <input type="text" id="roll_number" name="roll_number" required><br>
 
         <label for="full_name">Full Name:</label><br>
-        <input type="tel" id="full_name" name="full_name" required><br>
+        <input type="text" id="full_name" name="full_name" required><br>
 
         <label for="email">Email:</label><br>
         <input type="text" id="email" name="email" required><br>
 
         <label for="mobileno">Mobile Number:</label><br>
-        <input type="text" id="mobileno" name="mobileno" required><br>
+        <input type="tel" id="mobileno" name="mobileno" required><br>
 
         <input type="submit" value="Submit">
     </form>
